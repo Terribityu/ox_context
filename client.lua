219f6ab6fdb7c7fe33a21e14c6a973504d3de6cf
John Rey Sto Domingo <80687279+Terribityu@users.noreply.github.com>
2023-09-08 04:13:01 +0800

    Add files via upload

--- client.lua
+++ client.lua
@@ -217,6 +217,56 @@ lib.registerContext({
     }
   })
 
-  RegisterCommand('testcontext', function()
-    lib.showContext('citizen_menu')
-  end)
+  lib.registerContext({
+    id = 'police_documents_menu',
+    title = '📑 Police Documents Menu',
+    options = {
+      {
+        title = '🔫 Weapons Permit',
+        description = 'Special gun permit provided by the police.',
+        event = 'esx_documents:createnewdocument',
+        args = 1
+      },
+      {
+        title = '🚘 Student Driver Permit',
+        description = 'Special student driver permit by the police.',
+        event = 'esx_documents:createnewdocument',
+        args = 2
+      },
+      {
+        title = '👮🏻‍♂️ Police Clearance',
+        description = 'Official clean, general purpose, citizen criminal record.',
+        event = 'esx_documents:createnewdocument',
+        args = 3
+      }
+    }
+  })
+
+  lib.registerContext({
+    id = 'ambulance_documents_menu',
+    title = '📑 Ambulance Documents Menu',
+    options = {
+      {
+        title = '😷 Medical Report - Phatology',
+        description = 'Official medical report provided by a pathologist.',
+        event = 'esx_documents:createnewdocument',
+        args = 1
+      },
+      {
+        title = '😀 Medical Report - Psychology',
+        description = 'Official medical report provided by a psychologist.',
+        event = 'esx_documents:createnewdocument',
+        args = 2
+      },
+      {
+        title = '🤓 Medical Report - Eye Specialist',
+        description = 'Official medical report provided by an eye specialist.',
+        event = 'esx_documents:createnewdocument',
+        args = 3
+      }
+    }
+  })
+
+  -- RegisterCommand('testcontext', function()
+  --   lib.showContext('citizen_menu')
+  -- end)
